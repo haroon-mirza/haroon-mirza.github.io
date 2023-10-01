7e4a8ee7e687b82dfb4f74414c92c789b8d26123
haroon-mirza <amirzaharoon@gmail.com>
2023-09-30 22:55:12 -0400

    projects placeholder

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,11 @@ function Projects() {
   return (
     <section id="projects" className="projects">
       <h2>Projects</h2>
-      {/* List your projects here */}
+      <div className="project">
+        <h3>Project 1</h3>
+        <p>Description of Project 1.</p>
+      </div>
+      {/* Repeat for other projects */}
     </section>
   );
 }
