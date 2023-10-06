e3eaa2f8d9d94ae96ef945a6369900bf5a121818
haroon-mirza <amirzaharoon@gmail.com>
2023-10-05 23:46:02 -0400

    Updated for github pages deployment

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,9 +5,9 @@ function AboutMe() {
   return (
     <section id="about-me" className="about-me">
       <h2>About Me</h2>
-      <p>Hello, I'm Haroon, a developer interested in applying his skillsets wherever needed
+      <p>Hello, I'm Haroon, a developer interested in applying his skillsets wherever needed <br /> <br />
 
-I’m open to collaborations and opportunities and am always eager to tackle new challenges and solve problems.
+ Open to collaborations and opportunities and tackle new challenges and solve problems <br /> <br />
 
 Interested? Feel free to reach out to me via email
 </p>
